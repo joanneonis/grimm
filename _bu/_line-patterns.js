337eff541918b534131c8929fb890ecfebeb5762
joanne <joanneonis@gmail.com>
2019-09-18 14:17:00 +0200

    updates

--- _bu/_line-patterns.js
+++ _bu/_line-patterns.js
@@ -1,13 +1,12 @@
 import { loadJSON, pad } from './helpers/loadJSON';
 
 let storyIndex;
-let storyCount = 209; // 209
+let storyCount = 1; // 209
 let animalData;
 let stories = [];
 let storiesCombined = '';
 let animalCounts = [];
 
-const fullTextContainer = document.querySelector('.stories-combined');
 
 loadJSON('stories/index.json', (response) => {
   storyIndex = JSON.parse(response);
@@ -15,66 +14,52 @@ loadJSON('stories/index.json', (response) => {
 
 loadJSON('animals-2.json', (response) => {
   animalData = JSON.parse(response);
-  loadStories();
 }, 'json');
 
-function loadStories() {
-  for (let i = 0; i < storyCount; i++) {
-    loadJSON(`stories/${pad(i + 1, 3)}.txt`, (response) => {
-      // store seperate stories
-      stories.push(response);
-  
-      if (i === 0) {
-        console.log(response);
-        storiesCombined = `<span class="story"><span class="title">${storyIndex[i].title}</span>${response}</span>`
-      } else {
-        storiesCombined = `
-          ${storiesCombined}
-          <span class="story">
-            <span class="title">${storyIndex[i].title}</span>
-            ${response}
-          </span>
-        `;
-      }
-  
-      if (i === storyCount - 1) init();
-    });
-  }  
+for (let i = 3; i < 4; i++) {
+  loadJSON(`stories/${pad(i + 1, 3)}.txt`, (response) => {
+    const fullTextContainer = document.querySelector('.stories-combined');
+
+    // store seperate stories
+    stories.push(response);
+
+    // combine all stories
+    storiesCombined = storiesCombined + ' ' + response;
+    let compromise = window.nlp(storiesCombined).out('html');
+    console.log(compromise);
+
+    if (i === storyCount - 1) init();
+    fullTextContainer.innerHTML = compromise;
+  });
 }
 
-
 function init() {
-  fullTextContainer.innerHTML = storiesCombined;
-  listAnimals(animalData);
+  // fullTextContainer.innerHTML = storiesCombined;
+  // listAnimals(animalData);
 }
 
 //?-----------
 
 function highLight(target, textContainer, i) {
   var item = textContainer;
-  var text = item.innerHTML; // textcontent
+  var text = item.textContent;
   var featuredWords = item.querySelectorAll('.highlight');
   
-  console.log('current', target);
-  console.log(featuredWords);
   var words = Array.prototype.slice.call(featuredWords, 0).map(function(node) {
     return node.textContent;  
   })
   
-  // first highlight
-  var regex = new RegExp('\\b(' + target + ')\\b', 'ig', 'a');
+  var regex = new RegExp('\\b(' + target + ')\\b', 'ig');
+  
   text = text.replace(regex, `<span class="highlight">$1</span>`);
-  // text = text.replace(/target/g, "a");
-
   var countOccurances = ((text || '').match(regex) || []).length;
 
   animalCounts[i].count = countOccurances;
 
-  // put the previous words back 
-  // words.forEach(function(word) {
-  //   console.log(words);
-  //   text = text.replace(word, `<span class="highlight">${word}</span>`); 
-  // });
+  // put the previous words back
+  words.forEach(function(word) {
+    text = text.replace(word, `<span class="highlight">${word}</span>`); 
+  });
   
   item.innerHTML = text;
 }
